44e0f3432cc1708c7de940a7489eafa2dbf246e2
Marshal1l <1113584652@qq.com>
2025-12-02 13:24:32 +0800

    finish image-share func

--- src/runtime/sftp-build.sh
+++ src/runtime/sftp-build.sh
@@ -1,5 +1,5 @@
 export CC=aarch64-linux-musl-gcc
 export GOARCH=arm64 GOARM="" CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc && make
-cp -v kata-runtime ~/opencca-work/SFTP_folder/kata-bins/
-cp -v containerd-shim-kata-v2 ~/opencca-work/SFTP_folder/kata-bins/
-cp -v kata-monitor ~/opencca-work/SFTP_folder/kata-bins/
+cp -v kata-runtime ~/cca-sbsa/SFTP_folder/kata-bins/
+cp -v containerd-shim-kata-v2 ~/cca-sbsa/SFTP_folder/kata-bins/
+cp -v kata-monitor ~/cca-sbsa/SFTP_folder/kata-bins/
